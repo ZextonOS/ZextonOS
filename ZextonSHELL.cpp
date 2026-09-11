303ab5c35105267207992d433ab5f7fde4d23a4a
ZextonOS <zextonsupport@gmail.com>
2026-09-11 18:57:08 -0500

    Update ZextonSHELL.cpp

--- ZextonSHELL.cpp
+++ ZextonSHELL.cpp
@@ -25,7 +25,6 @@ int main() {
     std::system("clear");
     std::cout << "[SYSTEM PROCESS] - booting ZextonOS" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(6));
-    std::system("clear");
     std::cout << "welcome to ZextonOS" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(3));
     std::system("clear");
